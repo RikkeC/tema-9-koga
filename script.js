32fcae2838aaa15e58b0575c172ee20662ea2d62
maja669e <maja669e@stud.kea.dk>
2019-11-05 18:00:26 +0100

    i gang med kontakt siden

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ function toggleMenu() {
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+
+
+$(function scroll_button() {
+    $('a[href*=#]').on('click', function scroll_button(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
